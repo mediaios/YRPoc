f271a8b4b1888561a2fdfce781e48799573f334a
mediaios <iosmediadev@gmail.com>
2023-05-19 15:58:36 +0800

    supplementry subission

--- CusYrPoc/CusYrPoc/ViewController.m
+++ CusYrPoc/CusYrPoc/ViewController.m
@@ -6,17 +6,34 @@
 //
 
 #import "ViewController.h"
+#import "MiLiveViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong) MiLiveViewController *miLiveVC;
 @end
 
 @implementation ViewController
+
+- (MiLiveViewController *)miLiveVC
+{
+    if(!_miLiveVC){
+        _miLiveVC = [[MiLiveViewController alloc] initWithNibName:@"MiLiveViewController" bundle:nil];
+    }
+    return _miLiveVC;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onPressedBtnToLivePage:(id)sender {
+    [self presentViewController:self.miLiveVC animated:YES completion:nil];
+    
+}
+- (IBAction)onPressedBtnToCdn:(id)sender {
+}
+- (IBAction)onPressedBtnToRtc:(id)sender {
+}
 
 @end
